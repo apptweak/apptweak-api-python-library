adc59f34a19509ff8fd5394e176c13bae3bbb4e4
Renato "Lond" Cerqueira <renato@apptweak.com>
2020-09-10 12:04:25 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('DESCRIPTION.md') as f:
 
 setup(
     name="apptweak",
-    version="1.0.3",
+    version="1.0.4",
     description="access the apptweak-io API in an easier way",
     long_description=long_description,
     long_description_content_type='text/markdown', # This is important!
